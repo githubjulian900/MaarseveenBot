15d4d82421d255799f044cd30abe84395396f681
Jelko Smidt <BWB45@OWSA.NL>
2022-02-12 17:28:59 +0100

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,32 +1,4 @@
 
-const {Client, Discord, MessageEmbed, Intents} = require("discord.js")
-
-
-
-const client = new Client({
-	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES,
-	Intents.FLAGS.GUILD_MEMBERS]
-})
-
-
-module.exports.run = async (bot, message, args) => {
-const bottie = client
-const embed = new MessageEmbed()
-.setTitle("Info")
-.setDescription("BotInformatie")
-.setFields(
-	{name: "Botnaam", value: bottie.name}
-	)
-
-
-.setThumbnail(guild.iconURL())
-.setColor("RANDOM")
-
-message.reply({embeds: [embed]})
-    
-}   
-
-
 module.exports.help = {
 name: "botinfo",
 Category: "Info",

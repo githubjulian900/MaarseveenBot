f12f8c6326a6c0efb835b94ef4881216b2e4877a
Jelko Smidt <BWB45@OWSA.NL>
2022-02-12 16:06:04 +0100

    Kick + Index Updated

--- commands/kick.js
+++ commands/kick.js
@@ -2,17 +2,10 @@ const disocrd = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
 
-  
-    if (!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("Invalid Permissions")
-    let User = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
-    if (!User) return message.channel.send("Invalid User")
-    if (User.hasPermission("KICK_MEMBERS")) return message.reply("Invalid Permissions")
-    let kickreden = args.join(" ").slice(22);
-    if (!kickreden) {
-      kickreden = "None"
-    }
-    
-    User.kick({reason: kickreden})
+
+if(!message.author.hasPermissions("KICK_MEMBERS")) return message.channel.send("JIJ NIET MOGEN")
+
+
 }
 
 

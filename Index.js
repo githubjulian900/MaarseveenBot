d5db7822385f7b65793bd725281852c59c16b4dd
Jelko Smidt <BWB45@OWSA.NL>
2022-02-12 16:01:14 +0100

    Update Index.js

--- Index.js
+++ Index.js
@@ -58,6 +58,10 @@ channelWelkom.send({embeds: [welkomembed]})
 client.once("ready", () => {
 	console.log(client.user.username, 'is online')
 	client.user.setActivity("To Maarseveen", { type: "WATCHING" })
+
+	const logchannel = require("940231367986401280")
+
+	logchannel.send("Online")
 })
 
 

4bf5d979ee2a5dd1e92c03ff27ecc2b5487b0fc4
Julian Smidt <65156876+githubjulian900@users.noreply.github.com>
2022-02-16 13:39:54 +0100

    Update logo.js

--- commands/logo.js
+++ commands/logo.js
@@ -2,7 +2,7 @@
 module.exports.run = async (bot, message, args) => {
 const icon = message.guild.iconURL()
    
- if(!member.roles.cache.has('934125329109385277')  message.reply("je hebt de perms niet")
+ if(!member.roles.cache.has('934125329109385277')  
  if(member.roles.cache.has('934125329109385277') {
    message.reply(icon)
 

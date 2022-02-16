62613917a5d47f87c19c67552ea9925f81eaaba2
githubjulian900 <65156876+githubjulian900@users.noreply.github.com>
2022-02-16 11:55:01 +0100

    Update membercount.js

--- commands/membercount.js
+++ commands/membercount.js
@@ -22,7 +22,7 @@ module.exports.run = async (bot, message, args) => {
                 name: 'Membercount',
                 value: guild.memberCount.toString(),
                 inline: true
-              },
+              }
 
               })
 

9e183ba8b663dd599b3ba049f6c7410925388e1c
Jelko Smidt <BWB45@OWSA.NL>
2022-02-16 20:29:17 +0100

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,71 +1,67 @@
 
-const {Client, Discord, MessageEmbed, Intents} = require("discord.js")
+const { Client, Discord, MessageEmbed, Intents } = require("discord.js")
 
 
 
 const client = new Client({
-	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES,
-	Intents.FLAGS.GUILD_MEMBERS]
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES,
+  Intents.FLAGS.GUILD_MEMBERS]
 })
 
 
 module.exports.run = async (bot, message, args) => {
- const guild = message.guild
-	const owner = await message.guild.fetchOwner()   
-	const icon = guild.iconURL()
-    let guildDescription = guild.description
-    if (!guildDescription) {
-      guildDescription = 'None'
-    }
+  const guild = message.guild
+  const owner = await message.guild.fetchOwner()
+  const icon = guild.iconURL()
+  const name = guild.name
+  let guildDescription = guild.description
+  if (!guildDescription) {
+    guildDescription = 'None'
+  }
 
-    const embed = new MessageEmbed()
-    .setTitle('serverinfo')
-    	.setThumbnail(icon)
+  const embed = new MessageEmbed()
+    .setTitle(`${icon}, ${name}`)
+    .setThumbnail(icon)
     .addFields({
-                name: 'Name',
-                value: guild.name,
-                inline: true
-              },
-              {
-                name: 'ID',
-                value: guild.id,
-                inline: true
-              },
-          
-              {
-                name: 'Created at',
-                value: guild.createdAt.toDateString(),
-                inline: true
-              },
-              {
-                name: 'Owner',
-                value: owner.user.tag,
-                inline: true
-              },
-              {
-                name: 'Member Count',
-                value: guild.memberCount.toString(),
-                inline: true
-              },
-              {
-                name: 'Boosts',
-                value: guild.premiumSubscriptionCount.toString(),
-                inline: true
-              },
-              {
-                name: 'Boost Level',
-                value: guild.premiumTier,
-                inline: true
-              })
+      name: 'ID',
+      value: guild.id,
+      inline: true
+    },
 
-    message.reply({ embeds: [embed] })
+      {
+        name: 'Created at',
+        value: guild.createdAt.toDateString(),
+        inline: true
+      },
+      {
+        name: 'Owner',
+        value: owner.user.tag,
+        inline: true
+      },
+      {
+        name: 'Member Count',
+        value: guild.memberCount.toString(),
+        inline: true
+      },
+      {
+        name: 'Boosts',
+        value: guild.premiumSubscriptionCount.toString(),
+        inline: true
+      },
+      {
+        name: 'Boost Level',
+        value: guild.premiumTier,
+        inline: true
+      })
 
-}   
+  message.reply({ embeds: [embed] })
+
+}
 
 
 
 module.exports.help = {
-name: "serverinfo",
-Category: "Info",
-desciption: "info command" 
+  name: "serverinfo",
+  Category: "Info",
+  desciption: "info command"
 }   

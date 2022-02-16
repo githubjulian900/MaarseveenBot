85ed28716883d7d04a7d1d459719236bc67ba5c2
Julian Smidt <65156876+githubjulian900@users.noreply.github.com>
2022-02-16 13:56:18 +0100

    Update herstart.js

--- commands/herstart.js
+++ commands/herstart.js
@@ -1,5 +1,5 @@
 const { Permissions } = require('discord.js');
-
+const member = message.author
 if (member.permissions.has(Permissions.FLAGS.ADMINISTARTOR)) {
 	message.channel.send("Herstart in 10 sec")
   wait(10)

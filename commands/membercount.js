9adccbc57f964d1ef68a844f3a72230de6d4c0b0
githubjulian900 <65156876+githubjulian900@users.noreply.github.com>
2022-02-16 11:51:00 +0100

    Create membercount.js

---
+++ commands/membercount.js
@@ -0,0 +1,40 @@
+const {Client, Discord, MessageEmbed, Intents} = require("discord.js")
+
+
+
+const client = new Client({
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES,
+	Intents.FLAGS.GUILD_MEMBERS]
+})
+
+
+module.exports.run = async (bot, message, args) => {
+ const guild = message.guild
+	const owner = await message.guild.fetchOwner()    
+    let guildDescription = guild.description
+    if (!guildDescription) {
+      guildDescription = 'None'
+    }
+
+    const embed = new MessageEmbed()
+    .setTitle('Membercount')
+    .addFields({
+                name: 'Membercount',
+                value: guild.memberCount.toString(),
+                inline: true
+              },
+
+              })
+
+    message.reply({ embeds: [embed] })
+
+}   
+
+
+
+module.exports.help = {
+name: "serverinfo",
+Category: "Info",
+desciption: "info command" 
+}   
+

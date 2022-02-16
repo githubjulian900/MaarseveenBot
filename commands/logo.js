f33387942d75abf3255ed4f3be4e31e9a1a5e173
githubjulian900 <65156876+githubjulian900@users.noreply.github.com>
2022-02-16 13:34:04 +0100

    Update logo.js

--- commands/logo.js
+++ commands/logo.js
@@ -1,1 +1,18 @@
-a
+
+module.exports.run = async (bot, message, args) => {
+const icon = message.guild.iconURL()
+   
+ if(!member.roles.cache.has('934125329109385277');
+ if(member.roles.cache.has('934125329109385277') {
+   message.reply(icon)
+
+
+        
+}   
+
+
+module.exports.help = {
+name: "logo",
+Category: "General",
+desciption: "Hallo Command"
+}

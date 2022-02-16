38738da438dee75851a8155a1314dc5cfcb59f6a
githubjulian900 <65156876+githubjulian900@users.noreply.github.com>
2022-02-16 12:09:12 +0100

    Update playerjoin.js

--- events/playerjoin.js
+++ events/playerjoin.js
@@ -1,3 +1,18 @@
+module.exports.run = async (client, member) => {
+    console.log('Member Joined');
+    let channel = client.channels.cache.get('776942211798532106');
+
+    let { user } = member;
+
+    var name = user.tag;
+
+    let embed = new MessageEmbed()
+    .setTitle('Welcome')
+    .setDescription('Have a nice time, ' + name)
+    channel.send({embeds: [embed);
+
+}
+
 module.exports.help = {
 name: "playerjoin"
 }

1016ffe55f49931484ce4433d79e05b411a4ed78
Julian Smidt <65156876+githubjulian900@users.noreply.github.com>
2022-02-16 13:57:33 +0100

    a

--- commands/herstart.js
+++ commands/herstart.js
@@ -1,7 +1,22 @@
+
+
+module.exports.run = async (bot, message, args) => {
+
+   
 const { Permissions } = require('discord.js');
 const member = message.author
 if (member.permissions.has(Permissions.FLAGS.ADMINISTARTOR)) {
 	message.channel.send("Herstart in 10 sec")
   wait(10)
   message.channel.send("Herstart")
+}
+       
+        
+}   
+
+
+module.exports.help = {
+name: "herstart",
+Category: "General",
+desciption: "Hallo Command"
 }

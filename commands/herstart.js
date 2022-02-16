5df6df5b54a977782334fe519808e1e6c856b45e
Jelko Smidt <BWB45@OWSA.NL>
2022-02-16 16:41:13 +0100

    Update herstart.js

--- commands/herstart.js
+++ commands/herstart.js
@@ -3,7 +3,8 @@
 module.exports.run = async (bot, message, args) => {
   const discord = require("discord.js")
    
-
+  message.reply("Herstart over 10  sec")
+  await sleep(1000);
   message.reply("Herstart")
 }
        

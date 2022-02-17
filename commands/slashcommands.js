dacf5b6ae1a49ba0389a5bde8de1a54f8b795366
Julian Smidt <65156876+githubjulian900@users.noreply.github.com>
2022-02-17 10:33:24 +0100

    Update slashcommands.js

--- commands/slashcommands.js
+++ commands/slashcommands.js
@@ -1,28 +1,14 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
-const data = new SlashCommandBuilder()
-	.setName('echo')
-	.setDescription('Replies with your input!')
-	.addStringOption(option =>
-		option.setName('input')
-			.setDescription('The input to echo back')
-			.setRequired(true));
 
 
 module.exports.run = async (bot, message, args) => {
-
-   const { SlashCommandBuilder } = require('@discordjs/builders');
-
-const data = new SlashCommandBuilder()
-	.setName('echo')
-	.setDescription('Replies with your input!')
-	.addStringOption(option =>
-		option.setName('input')
-			.setDescription('The input to echo back')
-			.setRequired(true));
-
-       
-        
+data: new SlashCommandBuilder()
+	.setname("ping")
+	.setDesciption("PONG!")
+	async execute(messge) {
+	message.reply("PONG!")
+	}
 }   
 
 

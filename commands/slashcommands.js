5e58f1d1c761f49f1a08e38db94743ac5dfd568e
Julian Smidt <65156876+githubjulian900@users.noreply.github.com>
2022-02-17 10:18:14 +0100

    Update slashcommands.js

--- commands/slashcommands.js
+++ commands/slashcommands.js
@@ -7,3 +7,27 @@ const data = new SlashCommandBuilder()
 		option.setName('input')
 			.setDescription('The input to echo back')
 			.setRequired(true));
+
+
+module.exports.run = async (bot, message, args) => {
+
+   const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const data = new SlashCommandBuilder()
+	.setName('echo')
+	.setDescription('Replies with your input!')
+	.addStringOption(option =>
+		option.setName('input')
+			.setDescription('The input to echo back')
+			.setRequired(true));
+
+       
+        
+}   
+
+
+module.exports.help = {
+name: "slash",
+Category: "General",
+desciption: "Hallo Command"
+}

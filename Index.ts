7a8b43989025e71260be3ce4f5f6d7456b3a0211
Julian Smidt <65156876+githubjulian900@users.noreply.github.com>
2022-03-02 13:15:40 +0100

    Update Index.ts

--- Index.ts
+++ Index.ts
@@ -1,35 +1,52 @@
-import DiscordJS, { Intents } from "discord.js"
-
-
-const client = new DiscordJS.Client({
-intents: [
-Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES
-]
-)}
-
-client.on("ready" () => {
-console.log("Bot")
-const guildid = '928379498079084554'
-const guild = client.guilds.cache.get(guildid)
-let commands
-
-
-if(guild) {
-commands = guild.commands
-} else  {
-commands = application.commands
-}
-
+import discordJS, { Intents } from 'discord.js'
  
+const client = new discordJS.Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
 })
 
-client.on("messageCreate" (message) => {
-if(message.content === 'ping') {
-message.reply({
-content: 'pong'
-)}
-}
+client.on("ready", () => {
+    console.log("Ready");
+
+    const guildid = ("928379498079084554")
+    const guild = clinet.guilds.cache.get(guildid)
+
+
+    let commands
+
+    if(guild) {
+        commands = guild.commands
+    } else {
+        commands = client.application.commands
+    }
+
+    commands.create({
+        name: "Ping",
+        description: "pong"
+    })
+
+    commands.create({
+        name: "Plus Som",
+        description: "Maakt een plus som",
+        options: [
+            {
+                name: '',
+
+            }
+        ]
+    })
+})
+client.on("InteractionCreate", async (interaction) => {
+    if(interaction.Iscommand()) {
+        return
+    }
+    const { commandName, options} = interaction
+
+    if ( commandName === 'ping') {
+        interaction.reply({
+            content: 'pong',
+            ephemeral: true,
+        })
+    } else if ()
 })
 
 client.login(process.env.token)
-
